a3c325a76e9efb200530a1a90ffffe5bcf90db09
mcgovernl <lmcgovern@colgate.edu>
2019-03-30 13:14:56 -0400

    Set up Intial File

---
+++ distance_vector.py
@@ -0,0 +1,18 @@
+#!/bash/bin/python3
+
+from argparse import ArgumentParser #look at proj02 for help using this
+import os
+import sys
+import time
+import bisect #not sure we need this one
+
+class Simulation
+
+class Network
+
+class Switch
+
+def main():
+
+if __name__ == '__main__':
+    main()

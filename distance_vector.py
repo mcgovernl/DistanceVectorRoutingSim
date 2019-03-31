f8bbb8600a2fca6ad8b3e296938d720edfeb68d0
mcgovernl <lmcgovern@colgate.edu>
2019-03-31 14:26:47 -0400

    Setup

--- distance_vector.py
+++ distance_vector.py
@@ -7,12 +7,17 @@ import time
 import bisect #not sure we need this one
 
 class Simulation
+    def __init__(self,settings):
 
 class Network
+    def __init__(self,params):
 
 class Switch
+    def __init__(self):
+        self._fwdtable = {}
 
 def main():
+
 
 if __name__ == '__main__':
     main()

f2e8191e54aab378650452b623e5eace3d8ad783
mcgovernl <lmcgovern@colgate.edu>
2019-04-07 15:58:59 -0400

    send done, recv needs work

--- distance_vector.py
+++ distance_vector.py
@@ -9,30 +9,53 @@ import bisect #not sure we need this one
 class Simulation:
     def __init__(self,settings):
         self._settings = settings
-        self._switches = create_switches(settings.switches,settings.links,settings.hosts)
+        self._switches = create_switches(settings.switches,settings.links)
         self._links = settings.links
-        self._hosts = settings.hosts
+        self._inflightvectors = {} #key is where vector should be going, value is list of vectors
 
-    def create_switches(n,links,hosts):
+    def create_switches(n,links):
         switches = {}
         for i in range(n):
-            switches[i+1] = Switch(i+1,links,hosts)
+            switches[i] = Switch(i,links[i])
         return switches
+
+    def send_vectors():
+        for switch in switches:
+            if switch._updated:
+                for num in switch._links:
+                    if self._inflightvectors.has_key(num):
+                        self._inflightvectors[num].append(switch._vector)
+                    else:
+                        self._inflightvectors[num] = [switch._vector]
+            switch._updated = False
+
+    def recv_vectors():
+        for num in self._inflightvectors:
+            for vector in self._inflightvectors[num]:
+                self._switches[num].update_vector(vector)
+
+
 
     def step(self,t):
         #plan order of simulation steps here
+        recv_vectors()
+        send_vectors()
 
 
-
-class NetworkState:
-    def __init__(self,switches):
 
 class Switch:
-    def __init__(self,num,links,hosts):
+    def __init__(self,num,links):
         self._num = num
-        self._links = links[num-1]
-        self._hosts = hosts[num-1]
-        self._fwdtable = []
+        self._links = links
+        self._updated = True
+        self._vector = [[num,0]]
+
+    def update_vector(vector):
+        #should process and update vector, should set updated if there was an update
+        return None
+
+class NetworkState:
+    def __init__(self,switches,inflightvectors):
 
 def main():
     # Parse arguments
@@ -41,8 +64,6 @@ def main():
             type=int, default=3, help='Number of switches in network')
     arg_parser.add_argument('--links', dest='links', action='store',
             type=list, default=[[2,3],[3],[1,2]], help='2D list of switch connections')
-    arg_parser.add_argument('--hosts', dest='hosts', action='store',
-            type=list, default=[[1],[2],[3]], help='2D list of host connections')
     arg_parser.add_argument('--steps', dest='steps', action='store',
             type=int, default=10, help="How many time steps to simulate")
     arg_parser.add_argument('--speed', dest='speed', action='store',
